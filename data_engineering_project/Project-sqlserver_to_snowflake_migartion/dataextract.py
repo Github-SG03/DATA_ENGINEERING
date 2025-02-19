1eec132174ccc1564574260895707cd3eb54f745
Shivam Gupta <sgs.shivam99@outlook.com>
2025-02-19 10:25:42 +0530

    commit

---
+++ data_engineering_project/Project-sqlserver_to_snowflake_migartion/dataextract.py
@@ -0,0 +1,10 @@
+import pyodbc
+import pandas as pd
+
+# SQL Server connection
+conn = pyodbc.connect('DRIVER={SQL Server};SERVER=LAPTOP-2CRA05LG\SQLEXPRESS;DATABASE=AdventureWorks2022;UID=sgs@sqlserver;PWD=sgs99@sqlserver')
+query = "SELECT * FROM transactions"
+df = pd.read_sql(query, conn)
+
+# Save to CSV for staging or directly load into Snowflake
+df.to_csv('staging_transactions.csv', index=False)
